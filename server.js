724ceb26945f33aacfe943bee08e3dc2b5125d2f
Zephyr Pfeiffer <zephyrpfeiffer@gmail.com>
2022-09-27 13:58:19 -0400

    adjust layout of thought.ejs and add comments to style sheets

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// set up base page layouts
+// set up base page layouts for mythoughts.ejs and thought.ejs
 // make it to where users can only see posts they are authorized to look at
 // give ability to add tags to a post when creating one (later)
 
