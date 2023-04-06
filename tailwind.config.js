4f5811a30fa5625bd9ba40f4b3e24931ee6be3a8
ZephyrPfeiffer <zephyrpfeiffer@gmail.com>
2023-04-06 17:59:28 -0400

    convert from cdn tailwind to production tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.ejs"],
+  content: [
+    "./views/*.ejs",
+    "./public/js/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
