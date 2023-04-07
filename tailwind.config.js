deff14f5bd4e43340ada32a0f02a0c022d0c177a
ZephyrPfeiffer <zephyrpfeiffer@gmail.com>
2023-04-06 22:09:16 -0400

    fix styling issues from transferring over from cdn tailwind to production tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,15 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
+
 }
 
